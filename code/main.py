78f9852df77d927751b70753bbbf06b130bbd2cb
niv26 <nivohana@gmail.com>
2021-01-30 13:47:11 +0200

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,39 @@
+import vt
+import csv 
+import os 
+import glob
+dir_path = os.getcwd()
+csv_files_path = dir_path + '/files'
+
+
+# url , status , ts ,  harmless , malicious , suspicious , timeout , undetected 
+
+class db:
+    def __init__(self,username,password,database_name, url):
+        self.username = username
+        self.password = password
+        self.database_name = database_name
+        self.url = url 
+    
+    def read_target_csv_files(self, path )
+    
+
+
+
+
+# Create new entry in the DB
+# url , 
+
+
+
+def read_csv_file(path):
+
+
+
+
+# read csv file 
+# write urls into table 
+# test urls 
+#
+
+# url , status , ts ,  harmless , malicious , suspicious , timeout , undetected 
